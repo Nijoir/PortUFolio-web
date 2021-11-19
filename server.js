32764164823d4bb139927d2ce859b3cae45a2ec3
KarKazul <39073637+KarKazul@users.noreply.github.com>
2021-11-19 03:41:05 -0300

    nijoir up

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/PortUFolio'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/PortUFolio/index.html'));
 });
+
+app.listen(process.env.PORT || 5000);
